3397b6bf311c3262e04b79110a921472eedc3feb
weresquirrel <vermokuss@gmail.com>
2018-02-28 15:38:00 +0000

    register-login works at the backend

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 // const secureRoute = require('../lib/secureRoute');
 const compositions = require('../controllers/compositions');
+const auth = require('../controllers/auth');
 
 // routes go here
 router.route('/compositions')
@@ -11,6 +12,12 @@ router.route('/compositions/:id')
   .get(compositions.show)
   .put(compositions.update)
   .delete(compositions.delete);
+
+router.route('/register')
+  .post(auth.register);
+
+router.route('/login')
+  .post(auth.login);
 
 router.all('/*', (req, res) => res.notFound());
 
---
+++ models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+userSchema.set('toJSON', {
+  getters: true,
+  virtuals: true,
+  transform(obj, json) {
+    delete json._id;
+    delete json.__v;
+    delete json.password;
+  }
+});
+
+userSchema
+  .virtual('passwordConfirmation')
+  .set(function setPasswordConfirmation(passwordConfirmation) {
+    this._passwordConfirmation = passwordConfirmation;
+  });
+
+userSchema.pre('validate', function checkPassword(next) {
+  if(!this._passwordConfirmation || this._passwordConfirmation !== this.password) {
+    this.invalidate('passwordConfirmation', 'Passwords do not match');
+  }
+  next();
+});
+
+userSchema.pre('save', function hashPassword(next) {
+  if(this.isModified('password')) {
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+  }
+  next();
+});
+
+userSchema.methods.validatePassword = function validatePassword(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
