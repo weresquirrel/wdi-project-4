ee451fefd79fa51e909a44f307cd74b5c1deb0cd
weresquirrel <vermokuss@gmail.com>
2018-03-04 16:01:18 +0000

    static files on Heroku fixed

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(customResponses);
 
 app.use('/api', routes);
+app.use('/static', express.static(`${__dirname}/static`));
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 app.use(errorHandler);
