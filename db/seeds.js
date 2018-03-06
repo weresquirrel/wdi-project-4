32c72bf878248e278a9685ac2c92cc79f73fd769
weresquirrel <vermokuss@gmail.com>
2018-03-06 05:13:24 +0000

    seed file updated

--- db/seeds.js
+++ db/seeds.js
@@ -134,6 +134,16 @@ mongoose.connect(db[env])
         email: 'mike@mike.com',
         password: 'password',
         passwordConfirmation: 'password'
+      },{
+        username: 'Adam',
+        email: 'adam@adam.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+      },{
+        username: 'dancing_octopus',
+        email: 'd@o.com',
+        password: 'password',
+        passwordConfirmation: 'password'
       }]
     );
   })
@@ -147,21 +157,218 @@ mongoose.connect(db[env])
         sounds: [
           {
             id: sounds[1].id,
-            volume: 42
+            volume: 75
           },
           {
             id: sounds[0].id,
-            volume: 18
+            volume: 27
+          },
+          {
+            id: sounds[19].id,
+            volume: 14
+          },
+          {
+            id: sounds[5].id,
+            volume: 1
           }],
         createdBy: users[0].id
       },{
         title: 'Focus background',
         sounds: [{
           id: sounds[0].id,
-          volume: 72
+          volume: 24
+        },{
+          id: sounds[8].id,
+          volume: 51
+        },{
+          id: sounds[11].id,
+          volume: 28
         }],
         createdBy: users[1].id,
         private: true
+      },{
+        title: 'Pond',
+        sounds: [{
+          id: sounds[9].id,
+          volume: 42
+        },{
+          id: sounds[0].id,
+          volume: 39
+        },{
+          id: sounds[17].id,
+          volume: 78
+        },{
+          id: sounds[23].id,
+          volume: 43
+        }],
+        createdBy: users[3].id
+      },{
+        title: 'Summer Night',
+        sounds: [{
+          id: sounds[7].id,
+          volume: 47
+        },{
+          id: sounds[3].id,
+          volume: 19
+        },{
+          id: sounds[14].id,
+          volume: 41
+        },{
+          id: sounds[5].id,
+          volume: 15
+        },{
+          id: sounds[23].id,
+          volume: 14
+        }],
+        createdBy: users[0].id
+      },{
+        title: 'Home',
+        sounds: [{
+          id: sounds[24].id,
+          volume: 33
+        },{
+          id: sounds[17].id,
+          volume: 77
+        },{
+          id: sounds[23].id,
+          volume: 21
+        },{
+          id: sounds[16].id,
+          volume: 26
+        }],
+        createdBy: users[0].id
+      },{
+        title: 'open air cafe',
+        sounds: [{
+          id: sounds[2].id,
+          volume: 36
+        },{
+          id: sounds[1].id,
+          volume: 72
+        },{
+          id: sounds[23].id,
+          volume: 43
+        }],
+        createdBy: users[0].id
+      },{
+        title: 'Adam\'s super playlist',
+        sounds: [{
+          id: sounds[7].id,
+          volume: 100
+        },{
+          id: sounds[24].id,
+          volume: 54
+        },{
+          id: sounds[17].id,
+          volume: 53
+        },{
+          id: sounds[11].id,
+          volume: 46
+        },{
+          id: sounds[23].id,
+          volume: 53
+        },{
+          id: sounds[0].id,
+          volume: 42
+        },{
+          id: sounds[19].id,
+          volume: 34
+        }],
+        createdBy: users[2].id
+      },{
+        title: 'Urban mix',
+        sounds: [{
+          id: sounds[2].id,
+          volume: 31
+        },{
+          id: sounds[12].id,
+          volume: 62
+        },{
+          id: sounds[15].id,
+          volume: 59
+        },{
+          id: sounds[20].id,
+          volume: 51
+        }],
+        createdBy: users[1].id,
+        private: true
+      },{
+        title: 'machines + rain',
+        sounds: [{
+          id: sounds[22].id,
+          volume: 24
+        },{
+          id: sounds[8].id,
+          volume: 48
+        },{
+          id: sounds[4].id,
+          volume: 36
+        },{
+          id: sounds[6].id,
+          volume: 19
+        },{
+          id: sounds[11].id,
+          volume: 19
+        }],
+        createdBy: users[1].id
+      },{
+        title: 'Street',
+        sounds: [{
+          id: sounds[13].id,
+          volume: 12
+        },{
+          id: sounds[15].id,
+          volume: 71
+        },{
+          id: sounds[20].id,
+          volume: 73
+        },{
+          id: sounds[8].id,
+          volume: 30
+        },{
+          id: sounds[21].id,
+          volume: 87
+        }],
+        createdBy: users[1].id
+      },{
+        title: 'Cafe noises',
+        sounds: [{
+          id: sounds[10].id,
+          volume: 34
+        },{
+          id: sounds[18].id,
+          volume: 49
+        },{
+          id: sounds[8].id,
+          volume: 24
+        },{
+          id: sounds[15].id,
+          volume: 42
+        },{
+          id: sounds[20].id,
+          volume: 100
+        }],
+        createdBy: users[3].id,
+        private: true
+      },{
+        title: 'Buzz',
+        sounds: [{
+          id: sounds[22].id,
+          volume: 18
+        },{
+          id: sounds[4].id,
+          volume: 13
+        },{
+          id: sounds[6].id,
+          volume: 21
+        },{
+          id: sounds[16].id,
+          volume: 39
+        },{
+          id: sounds[3].id,
+          volume: 20
+        }],
+        createdBy: users[1].id
       }]
     );
   })
